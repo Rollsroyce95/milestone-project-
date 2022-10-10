7223c1ec21c1f90674fb88f0a251b5a8c78ddddc
Royce Childers <105823118+Rollsroyce95@users.noreply.github.com>
2022-10-10 16:05:23 -0700

    Update alien.js

--- alien.js
+++ alien.js
@@ -1,5 +1,5 @@
 
-const alienInMotion = npcAlien(-590,300)
+const alienInMotion = npcAlien(-590,340)
 
 function greyAlien(url){
     let grey = document.createElement('img')
@@ -150,7 +150,7 @@ function sleep(time){
 }
 
 async function readyAlienOne(){
-    await alienInMotion.flyEast(1400);
+    await alienInMotion.flyEast(1300);
     await alienInMotion.flySouth1(65);
     await alienInMotion.flySouth2(110);
     await alienInMotion.flySouth3(120);
