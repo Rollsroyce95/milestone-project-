94d86621e1e2a98bc13c6198293efa20ab125ef0
Royce Childers <105823118+Rollsroyce95@users.noreply.github.com>
2022-10-10 12:38:11 -0700

    Create ufo.js

---
+++ ufo.js
@@ -0,0 +1,73 @@
+shipInMotion = npcShip(-900,400)
+
+
+function alienShip(url){
+    let ship = document.createElement('img')
+    ship.src = url
+   
+    ship.style.position = 'fixed'
+    document.body.append(ship)
+    return ship
+}
+
+function npcShip(x,y){
+    let shipElement = alienShip('vecteezy_ufo-spaceship-concept-clipart-design-illustration_9356452_47.png')
+    shipElement.height = 800;
+    shipElement.style.zIndex = 1;
+    
+   
+  
+    let direction = null;
+
+    function moveShip (){
+        if(direction === "east"){
+        x += 8
+        }
+    
+        if(direction === "north"){
+        y += 6
+        }
+
+        shipElement.style.left = x + "px"
+        shipElement.style.bottom = y + "px"
+
+    }
+  
+    setInterval(moveShip,1)
+
+    async function flyEast(time){
+        direction = "east"
+        shipElement.src = "vecteezy_ufo-spaceship-concept-clipart-design-illustration_9356452_47.png"
+        await sleep(time)
+        stop()
+    }
+    async function flyNorth(time){
+        direction = "north"
+        shipElement.src = "vecteezy_ufo-spaceship-concept-clipart-design-illustration_9356452_47.png"
+        await sleep(time)
+        stop()
+    }
+    function stop(){
+        direction = null
+        shipElement.src = "vecteezy_ufo-spaceship-concept-clipart-design-illustration_9356452_47.png"
+    }
+
+    return{
+        shipElement:shipElement,
+        flyEast:flyEast,
+        flyNorth:flyNorth,
+        stop:stop,
+    }
+}
+function sleep(time){
+    return new Promise(resolve => {
+        setTimeout(resolve, time)
+    })
+}
+
+async function readyShipOne(){
+    await shipInMotion.flyEast(1050)
+
+}
+
+    readyShipOne()
