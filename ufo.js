a4acbdc096b85e99f05abbd6f3db54a4c136f884
Royce Childers <105823118+Rollsroyce95@users.noreply.github.com>
2022-10-10 12:00:21 -0700

    Update ufo.js

--- ufo.js
+++ ufo.js
@@ -13,7 +13,7 @@ function alienShip(url){
 }
 
 function npcShip(x,y){
-    let shipElement = alienShip('https://github.com/Rollsroyce95/milestone-project-/blob/bedeab1acd47fa48f2a28cdaacb4f877ca5caacf/x2ww_5npv_210520.jpg')
+    let shipElement = alienShip('https://raw.githubusercontent.com/Rollsroyce95/milestone-project-/main/x2ww_5npv_210520.jpg')
     shipElement.height = 350;
     shipElement.style.zIndex = 1;
     
@@ -39,19 +39,19 @@ function npcShip(x,y){
 
     async function flyEast(time){
         direction = "east"
-           let shipElement = alienShip('https://github.com/Rollsroyce95/milestone-project-/blob/bedeab1acd47fa48f2a28cdaacb4f877ca5caacf/x2ww_5npv_210520.jpg')
+           let shipElement = alienShip('https://raw.githubusercontent.com/Rollsroyce95/milestone-project-/main/x2ww_5npv_210520.jpg')
         await sleep(time)
         stop()
     }
     async function flyNorth(time){
         direction = "north"
-        let shipElement = alienShip('https://github.com/Rollsroyce95/milestone-project-/blob/bedeab1acd47fa48f2a28cdaacb4f877ca5caacf/x2ww_5npv_210520.jpg')
+        let shipElement = alienShip('https://raw.githubusercontent.com/Rollsroyce95/milestone-project-/main/x2ww_5npv_210520.jpg')
         await sleep(time)
         stop()
     }
     function stop(){
         direction = null
-          let shipElement = alienShip('https://github.com/Rollsroyce95/milestone-project-/blob/bedeab1acd47fa48f2a28cdaacb4f877ca5caacf/x2ww_5npv_210520.jpg')
+          let shipElement = alienShip('https://raw.githubusercontent.com/Rollsroyce95/milestone-project-/main/x2ww_5npv_210520.jpg')
     }
 
     return{
