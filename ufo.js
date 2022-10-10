1f0853f80b4291068d4bc3476cd558bfcf8d4af2
Royce Childers <105823118+Rollsroyce95@users.noreply.github.com>
2022-10-10 11:17:49 -0700

    Update ufo.js

--- ufo.js
+++ ufo.js
@@ -1,64 +1,45 @@
 
 
  
-  shipInMotion = npcShip(-900,400)
-
+ shipInMotion = npcShip(-750,565)
+//attribution:"https://opengameart.org/users/alucard"
 
 function alienShip(url){
     let ship = document.createElement('img')
     ship.src = url
-   
     ship.style.position = 'fixed'
     document.body.append(ship)
     return ship
 }
-
 function npcShip(x,y){
-    let shipElement = alienShip('https://raw.githubusercontent.com/Rollsroyce95/milestone-project-/main/—Pngtree—ufo_7077225.png')
-    shipElement.height = 800;
-    shipElement.style.zIndex = -1;
-    
+    let shipElement = alienShip('https://raw.githubusercontent.com/Rollsroyce95/milestone-project-/main/%E2%80%94Pngtree%E2%80%94ufo_7077225.png')
+    shipElement.height = 900;
+    shipElement.style.zIndex = 1;
    
   
     let direction = null;
-
     function moveShip (){
         if(direction === "east"){
-        x += 8
+        x += 6
         }
-    
-        if(direction === "north"){
-        y += 6
-        }
-
         shipElement.style.left = x + "px"
         shipElement.style.bottom = y + "px"
-
     }
   
     setInterval(moveShip,1)
-
     async function flyEast(time){
         direction = "east"
-       let shipElement = alienShip('https://raw.githubusercontent.com/Rollsroyce95/milestone-project-/main/—Pngtree—ufo_7077225.png')
-        await sleep(time)
-        stop()
-    }
-    async function flyNorth(time){
-        direction = "north"
-          let shipElement = alienShip('https://raw.githubusercontent.com/Rollsroyce95/milestone-project-/main/—Pngtree—ufo_7077225.png')
+        shipElement.src = "https://raw.githubusercontent.com/Rollsroyce95/milestone-project-/main/%E2%80%94Pngtree%E2%80%94ufo_7077225.png"
         await sleep(time)
         stop()
     }
     function stop(){
         direction = null
-      let shipElement = alienShip('https://raw.githubusercontent.com/Rollsroyce95/milestone-project-/main/—Pngtree—ufo_7077225.png')
+        shipElement.src = "https://raw.githubusercontent.com/Rollsroyce95/milestone-project-/main/%E2%80%94Pngtree%E2%80%94ufo_7077225.png"
     }
-
     return{
         shipElement:shipElement,
         flyEast:flyEast,
-        flyNorth:flyNorth,
         stop:stop,
     }
 }
@@ -67,10 +48,7 @@ function sleep(time){
         setTimeout(resolve, time)
     })
 }
-
 async function readyShipOne(){
-    await shipInMotion.flyEast(1050)
-
+    await shipInMotion.flyEast(2200)
 }
-
     readyShipOne()
